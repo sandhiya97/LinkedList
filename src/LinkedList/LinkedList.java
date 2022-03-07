7391745e4bba1d2ff6b48a9f145eaa423cfd61de
sandhiya97 <sandhiya.baburao@gmail.com>
2022-03-07 06:32:49 +0530

    Implementing Append function as per the problem statement

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,16 +1,26 @@
+package LinkedList;
 public class LinkedList {
 
     /**
-     * addNode - this method is created to add data to linedList
+     * addNode - this method is created to add data to linkedList
      *
      * @param head - accepting head pointer of LinkedList to manipulate it
-     * @param data - accepting data to put it in linkedList
+     * @param data - accepting data to create new node and add it in linkedList
      * @return head - it returns head of linkedList
      *
      */
     public Node addNode(Node head, int data) {
+        /*
+         * New node is created with the parameter data
+         */
         Node newNode = new Node(data);
 
+        /*
+         * If the param head is null, new node will be head
+         * else a separate pointer is created with head
+         * and pointer will be moved to last node in the list with while loop
+         * after reaching last node, it's next will be assigned to new node that was created earlier
+         */
         if (head == null) {
             head = newNode;
         } else {
@@ -22,41 +32,64 @@ public class LinkedList {
 
             pointer.next = newNode;
         }
-
         return head;
     }
 
-    public void printLinkedList(Node head) {
+    /**
+     * addNodeAsHead - Node will be added to head instead of tail
+     * @param head - accepting head pointer of LinkedList to manipulate it
+     * @param data - accepting data to create new node and add it in linkedList
+     * @return head - it returns head of linkedList
+     */
+    public Node addNodeAsHead(Node head, int data ){
+        /*
+         * New node is created with the parameter data
+         */
+        Node newNode = new Node(data);
+        /*
+         * If the list already have a head, new node's next will point to head.
+         * And head of the linkedList will be changed to new node.
+         */
+        if (head != null) {
+            newNode.next = head;
+        }
+
+        head = newNode;
+        return head;
+    }
+
+    /**
+     * appendNode - this method is created to append data to linkedList
+     *
+     * @param head - accepting head pointer of LinkedList to manipulate it
+     * @param data - accepting data to create new node and add it in linkedList
+     * @return head - it returns head of linkedList
+     *
+     */
+    public Node appendNode(Node head, int data) {
+        /*
+         * New node is created with the parameter data
+         */
+        Node newNode = new Node(data);
+
+        /*
+         * If the param head is null, new node will be head
+         * else a separate pointer is created with head
+         * and pointer will be moved to last node in the list with while loop
+         * after reaching last node, it's next will be assigned to new node that was created earlier
+         */
         if (head == null) {
-            System.out.println("LinkedList is empty");
+            head = newNode;
         } else {
             Node pointer = head;
-            while (pointer != null) {
-                System.out.print(pointer.data + " ");
+
+            while (pointer.next != null) {
                 pointer = pointer.next;
             }
-        }
-    }
 
-    public Node deleteNodeFromStart(Node head) {
-        System.out.println("Deleted node from start : " + head.data);
-        head = head.next;
+            pointer.next = newNode;
+        }
         return head;
     }
 
-    public void deleteNodeFromEnd(Node head) {
-        Node pointer = head;
-        Node prev = null;
-
-        while (pointer.next != null) {
-            prev = pointer;
-            pointer = pointer.next;
-        }
-        System.out.println("Deleted node from end : " + pointer.data);
-        prev.next = null;
-    }
-
-    public void deleteFromSpecificLocation(Node head, int positionOfNodeToBeDeleted) {
-
-    }
 }
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,68 +1,43 @@
 package LinkedList;
 
 public class Main {
-	/**
-	* Main method for manipulation linkedList
-	* @param args - Default Java param (Not used)
-	*/
-	public static void main(String[] args) {
-	// TODO Auto-generated method stub
+    /**
+     * Main method for manipulation linkedList
+     * @param args - Default Java param (Not used)
+     */
+    public static void main(String[] args) {
+        /*
+         * PROCEDURE
+         * 1. Creating a linkedList
+         * 2. Adding nodes to linkedList
+         * 3. Adding nodes to linkedList in head
+         * 4. Appending new nodes to end of linkedList
+         */
 
-	/*
-	* PROCEDURE
-	* 1. Creating Lined list
-	* 2. Adding nodes to linkedList
-	* 3. Printing linedList after adding nodes
-	* 4. Deleting node from start of the linkedlist
-	* 5. Printing linedList after deleting node from start of the linkedlist
-	* 6. Deleting node from end of the linkedlist
-	* 7. Printing linedList after deleting node from end of the linkedlist
-	*/
+        /*
+         * 1. Creating a linkedList
+         */
+        LinkedList list = new LinkedList();
 
-	/*
-	* 1. Creating Lined list
-	*/
-	LinkedList list = new LinkedList();
+        /*
+         * 2. Adding nodes to linkedList as in UC1
+         */
+        Node head1 = list.addNode(null, 56);
+        list.addNode(head1, 30);
+        list.addNode(head1, 70);
 
-	/*
-	* 2. Adding nodes to linkedList
-	*/
-	Node head = list.addNode(null, 1);
-	list.addNode(head, 2);
-	list.addNode(head, 3);
-	list.addNode(head, 4);
-	list.addNode(head, 5);
-	list.addNode(head, 6);
-	list.addNode(head, 7);
-	list.addNode(head, 8);
-	list.addNode(head, 9);
+        /*
+         * 3. Adding nodes as head to the linkedList as in UC2
+         */
+        Node head2 = list.addNodeAsHead(null, 70);
+        head2 = list.addNodeAsHead(head2, 30);
+        head2 = list.addNodeAsHead(head2, 56);
 
-	/*
-	* 3. Printing linedList after adding nodes
-	*/
-	list.printLinkedList(head);
-	System.out.println();
-
-	/*
-	* 4. Deleting node from start of the linkedlist
-	*/
-	head = list.deleteNodeFromStart(head);
-
-	/*
-	* 5. Printing linedList after deleting node from start of the linkedlist
-	*/
-	list.printLinkedList(head);
-	System.out.println();
-
-	/*
-	* 6. Deleting node from end of the linkedlist
-	*/
-	list.deleteNodeFromEnd(head);
-
-	/*
-	* 7. Printing linedList after deleting node from end of the linkedlist
-	*/
-	list.printLinkedList(head);
-
-	}
+        /*
+         * 4. Appending new nodes to end of linkedList as in UC3
+         */
+        Node head3 = list.appendNode(null, 56);
+        list.appendNode(head3, 30);
+        list.appendNode(head3, 70);
+    }
 }
