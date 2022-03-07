f0d3755764158702b27e6e2445a5d5a63f75ee01
sandhiya97 <sandhiya.baburao@gmail.com>
2022-03-07 06:19:46 +0530

    Update Main.java

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,68 +1,27 @@
 package LinkedList;
 
 public class Main {
-	/**
-	* Main method for manipulation linkedList
-	* @param args - Default Java param (Not used)
-	*/
-	public static void main(String[] args) {
-	// TODO Auto-generated method stub
+    /**
+     * Main method for manipulation linkedList
+     * @param args - Default Java param (Not used)
+     */
+    public static void main(String[] args) {
+        /*
+         * PROCEDURE
+         * 1. Creating a linkedList
+         * 2. Adding nodes to linkedList
+         */
 
-	/*
-	* PROCEDURE
-	* 1. Creating Lined list
-	* 2. Adding nodes to linkedList
-	* 3. Printing linedList after adding nodes
-	* 4. Deleting node from start of the linkedlist
-	* 5. Printing linedList after deleting node from start of the linkedlist
-	* 6. Deleting node from end of the linkedlist
-	* 7. Printing linedList after deleting node from end of the linkedlist
-	*/
+        /*
+         * 1. Creating a linkedList
+         */
+        LinkedList list = new LinkedList();
 
-	/*
-	* 1. Creating Lined list
-	*/
-	LinkedList list = new LinkedList();
-
-	/*
-	* 2. Adding nodes to linkedList
-	*/
-	Node head = list.addNode(null, 1);
-	list.addNode(head, 2);
-	list.addNode(head, 3);
-	list.addNode(head, 4);
-	list.addNode(head, 5);
-	list.addNode(head, 6);
-	list.addNode(head, 7);
-	list.addNode(head, 8);
-	list.addNode(head, 9);
-
-	/*
-	* 3. Printing linedList after adding nodes
-	*/
-	list.printLinkedList(head);
-	System.out.println();
-
-	/*
-	* 4. Deleting node from start of the linkedlist
-	*/
-	head = list.deleteNodeFromStart(head);
-
-	/*
-	* 5. Printing linedList after deleting node from start of the linkedlist
-	*/
-	list.printLinkedList(head);
-	System.out.println();
-
-	/*
-	* 6. Deleting node from end of the linkedlist
-	*/
-	list.deleteNodeFromEnd(head);
-
-	/*
-	* 7. Printing linedList after deleting node from end of the linkedlist
-	*/
-	list.printLinkedList(head);
-
-	}
+        /*
+         * 2. Adding nodes to linkedList as in UC1
+         */
+        Node head = list.addNode(null, 56);
+        list.addNode(head, 30);
+        list.addNode(head, 70);
+    }
 }
